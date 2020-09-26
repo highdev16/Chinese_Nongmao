d129d51899233381b1f5ca7bf63f0877aeff7b4b
WQ <SUNREDMOBILE11@Yandex.com>
2020-09-26 15:54:02 +0800

    OK

--- N3/p19.php
+++ N3/p19.php
@@ -2,7 +2,13 @@
 $bigTitle = "智能菜场";
 include('../N1/header.php');
 ?>
-<img src="images/bkg.jpg" alt="" class="u-expanded-width u-image u-image-default u-image-1" data-image-width="1001" data-image-height="439" style='width: 100%'>
+<style>
+  p.detail-text {
+    line-height: 50px;
+    font-size: 22px;
+  }
+</style>
+    <img src="images/bkg.jpg" alt="" class="u-expanded-width u-image u-image-default u-image-1" data-image-width="1001" data-image-height="439" style='width: 100%'>
     <section class="u-clearfix u-section-2" id="sec-3314">
       <div class="u-clearfix u-sheet u-sheet-1" style='min-height: 0px'>
         <div class="u-clearfix u-expanded-width u-gutter-10 u-layout-wrap u-layout-wrap-1" style='margin-bottom: 0px'>
@@ -32,7 +38,7 @@ include('../N1/header.php');
             <div class="u-layout-row">
               <div class="u-size-30">
                 <div class="u-layout-col">
-                  <div class="u-container-style u-image u-layout-cell u-left-cell u-size-60 u-image-1" src="" data-image-width="1778" data-image-height="712">
+                  <div class="u-container-style u-image u-layout-cell u-left-cell u-size-60 u-image-1" src="" data-image-width="1778" data-image-height="712" style='width:100%'>
                     <div class="u-container-layout u-valign-middle u-container-layout-1"></div>
                   </div>
                 </div>
@@ -41,14 +47,14 @@ include('../N1/header.php');
                 <div class="u-layout-row">
                   <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-60 u-layout-cell-2">
                     <div class="u-container-layout u-valign-middle u-container-layout-2">
-                      <p class="u-text u-text-1"> 1.城市智能菜场管理平台的建立&nbsp;&nbsp;<br>（1）商户管理 
-（2）监测管理 
-（3）追溯管理 
-（4）摊位管理 
-（5）水电管理 
-（6）消防管理
-（7）巡查管理 
-（8）智能停车管理 
+                      <p class="u-text u-text-1 detail-text"> 1. 城市智能菜场管理平台的建立&nbsp;&nbsp;<br>(1) 商户管理 
+(2) 监测管理 
+(3) 追溯管理 
+(4) 摊位管理 
+(5) 水电管理 
+(6) 消防管理
+(7) 巡查管理 
+(8) 智能停车管理 
                       </p>
                     </div>
                   </div>
@@ -62,7 +68,7 @@ include('../N1/header.php');
             <div class="u-layout-row">
               <div class="u-size-30">
                 <div class="u-layout-col">
-                  <div class="u-container-style u-image u-layout-cell u-left-cell u-size-60 u-image-2" src="" data-image-width="1422" data-image-height="569">
+                  <div class="u-container-style u-image u-layout-cell u-left-cell u-size-60 u-image-2" src="" data-image-width="1422" data-image-height="569" style='width:100%'>
                     <div class="u-container-layout u-valign-middle u-container-layout-3"></div>
                   </div>
                 </div>
@@ -71,7 +77,7 @@ include('../N1/header.php');
                 <div class="u-layout-row">
                   <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-60 u-layout-cell-4">
                     <div class="u-container-layout u-valign-middle u-container-layout-4">
-                      <p class="u-text u-text-2">&nbsp; 2.城市农贸电商平台的建立&nbsp;&nbsp;<br>城市农贸电商o2o平台实现全程配送，半小时到家，让消费者足不出户也能吃到安全放心的产品。
+                      <p class="u-text u-text-2 detail-text">2. 城市农贸电商平台的建立<br>城市农贸电商o2o平台实现全程配送，半小时到家，让消费者足不出户也能吃到安全放心的产品。
                       </p>
                     </div>
                   </div>
@@ -85,7 +91,7 @@ include('../N1/header.php');
             <div class="u-layout-row">
               <div class="u-size-30">
                 <div class="u-layout-col">
-                  <div class="u-container-style u-image u-layout-cell u-left-cell u-size-60 u-image-3" src="" data-image-width="1074" data-image-height="712">
+                  <div class="u-container-style u-image u-layout-cell u-left-cell u-size-60 u-image-3" src="" data-image-width="1074" data-image-height="712" style='width:100%'>
                     <div class="u-container-layout u-valign-middle u-container-layout-5"></div>
                   </div>
                 </div>
@@ -94,10 +100,10 @@ include('../N1/header.php');
                 <div class="u-layout-row">
                   <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-60 u-layout-cell-6">
                     <div class="u-container-layout u-valign-middle u-container-layout-6">
-                      <p class="u-text u-text-3"> 3.城市智能监管平台的建立&nbsp;<br>（1）市场规范化和标准化的建设；
-（2）监测规范和监控资料自动上传；
-（3）建立全市农产品电子追溯系统；
-（4）建立智能分析大数据分析系统； 
+                      <p class="u-text u-text-3 detail-text">3. 城市智能监管平台的建立&nbsp;<br>(1) 市场规范化和标准化的建设；
+(2) 监测规范和监控资料自动上传；
+(3) 建立全市农产品电子追溯系统；
+(4) 建立智能分析大数据分析系统； 
                       </p>
                     </div>
                   </div>
