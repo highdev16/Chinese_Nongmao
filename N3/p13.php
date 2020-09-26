dd2298f75f9c8348ba05a1286ff5eb9d4c65ec2a
WQ <SUNREDMOBILE11@Yandex.com>
2020-09-26 11:32:46 +0800

    OK

--- N3/p13.php
+++ N3/p13.php
@@ -12,6 +12,23 @@ include('../N1/header.php');
     width: 1500px;
     margin: 0 auto;
   }
+  section.u-section-2 {
+    height: 500px;
+  }
+  p.boss-detail-text {
+    line-height: 70px;
+    margin-top: -383px !important;
+  }
+  .u-section-5 .u-image-1, .u-section-5 .u-layout-cell-2 {
+    height: 400px;
+    min-height: 400px;
+  }
+  .u-section-5 {
+    min-height: 400px;
+  }
+  .u-section-5 .u-text-1 {
+    margin-top: 50px;
+  }
 </style>
     <section class="u-align-left u-clearfix u-image u-shading u-section-2" src="" id="sec-a760">
       <div class="u-clearfix u-sheet u-sheet-1"></div>
@@ -21,8 +38,8 @@ include('../N1/header.php');
         <div class="u-container-style u-grey-40 u-group u-group-1">
         </div>
         <img src="images/23.jpg" alt="" class="u-image u-image-default u-image-1">
-        <p class="u-text u-text-1">&nbsp; &nbsp;传统的农贸市场的业态单一，消费群体单一，消费欲望不强，市场环境差，随着社会文明进步的发展，显而易见，传统的农贸俨然已经无法满足当代年轻人的消费需求，传统单一的农贸市场已经逐步被淘汰。<br>
-          <br>&nbsp; &nbsp;光影集团董事会主席陈志勇在从事农贸行业20余年，通过对农贸市场建设工作的长期积累，考察调研以及制定相关的改造方案与实践，在全国率先提出符合农贸市场发展未来趋势的农贸市场行形态--农贸市场商业综合体。菜源佳佳开创了中国农贸市场商业综合体的新模式。&nbsp;&nbsp;
+        <p class="u-text u-text-1 boss-detail-text">传统的农贸市场的业态单一，消费群体单一，消费欲望不强，市场环境差，随着社会文明进步的发展，显而易见，传统的农贸俨然已经无法满足当代年轻人的消费需求，传统单一的农贸市场已经逐步被淘汰。<br>
+          <br>光影集团董事会主席陈志勇在从事农贸行业20余年，通过对农贸市场建设工作的长期积累，考察调研以及制定相关的改造方案与实践，在全国率先提出符合农贸市场发展未来趋势的农贸市场行形态--农贸市场商业综合体。菜源佳佳开创了中国农贸市场商业综合体的新模式。
         </p>
       </div>
     </section>
@@ -30,8 +47,8 @@ include('../N1/header.php');
       <div class="u-clearfix u-sheet u-sheet-1">
         <h2 class="u-align-center u-heading-font u-subtitle u-text u-text-1 yahei">农贸市场综合体</h2>
         <img src="images/ScreenShot2020-08-11at5.50.02AM.png" alt="" class="u-image u-image-default u-image-1" data-image-width="1207" data-image-height="28">
-        <p class="u-text u-text-2">&nbsp; &nbsp;农贸综合体就是农贸市场与商业综合或社区综合体等其他商业完美结合为“农贸+“，以农贸市场为主导，合理搭配商业或社区服务的新型组合模式，可以由农贸市场、生活广场、小商品市场、社区服务、超市等类别组成。<br>
-          <br>&nbsp; &nbsp;帮助农贸市场挖掘亮点，鼓励引导农贸市场走出特色化发展之路，实现传统农贸市场的现代化、特色化、多元化；让农贸市场超市化、企业化、品牌化，同时让消费者体验“一站式”吃喝玩乐，具备就近购物、餐饮服务、综合服务等服务多样的商业特征，增强消费者的幸福感；并且全面提升农贸市场基础设施、购物环境、交易条件和管理水平，为人民群众营造整洁、有序、安全的消费购物环境。
+        <p class="u-text u-text-2">农贸综合体就是农贸市场与商业综合或社区综合体等其他商业完美结合为“农贸+“，以农贸市场为主导，合理搭配商业或社区服务的新型组合模式，可以由农贸市场、生活广场、小商品市场、社区服务、超市等类别组成。<br>
+          <br>帮助农贸市场挖掘亮点，鼓励引导农贸市场走出特色化发展之路，实现传统农贸市场的现代化、特色化、多元化；让农贸市场超市化、企业化、品牌化，同时让消费者体验“一站式”吃喝玩乐，具备就近购物、餐饮服务、综合服务等服务多样的商业特征，增强消费者的幸福感；并且全面提升农贸市场基础设施、购物环境、交易条件和管理水平，为人民群众营造整洁、有序、安全的消费购物环境。
         </p>
         <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
           <div class="u-layout">
@@ -69,8 +86,8 @@ include('../N1/header.php');
             <div class="u-container-style u-grey-30 u-layout-cell u-right-cell u-size-30 u-size-xs-60 u-layout-cell-2">
               <div class="u-container-layout u-container-layout-2">
                 <h2 class="u-align-center u-text u-text-1 yahei">综合体运营</h2>
-                <p class="u-align-left u-text u-text-2">&nbsp; &nbsp; 我们按照万达商业的团队和万达商业综合体的制度以及万达双效的管理模式进行综合管理，<br>
-                  <br>&nbsp; &nbsp; &nbsp;综合运营。这是我们菜源佳佳未来主要核心推广的合作发展模式。 
+                <p class="u-align-left u-text u-text-2">  我们按照万达商业的团队和万达商业综合体的制度以及万达双效的管理模式进行综合管理，<br>
+                  <br>  综合运营。这是我们菜源佳佳未来主要核心推广的合作发展模式。 
                 </p>
               </div>
             </div>
@@ -78,5 +95,5 @@ include('../N1/header.php');
         </div>
       </div>
     </section>
-    
+    <div style='margin-top: 50px; width: 100%;'></div>
     <?php include('../N1/footer.php'); ?>
