2526016aac36571b40324b984a5b66b22a0e817a
WQ <SUNREDMOBILE11@Yandex.com>
2020-09-27 14:18:21 +0800

    OK

--- N4/p34.php
+++ N4/p34.php
@@ -8,13 +8,24 @@ include('../N1/header.php');
     flex-direction: 'row';
   }
   div.boss-profile div.board-content {
-    width: calc(100% - 400px)
+    width: calc(100% - 550px)
   }
 
   #sec-275e {
     min-height: 0px;
   }
+  .center-align {
+    margin-left: auto !important;
+    margin-right: auto !important;
+  }
 </style>
+  <div style='width: 100%; margin-top: 50px; text-align: center'>
+    <h2 class="u-align-center u-text u-text-default u-text-1 center-align">
+      <a href="http://www.gygygy.com/us.html?pageid=169611" target="http://www.gygygy.com/" class='yahei'>董事长简介</a>
+    </h2>
+    <p class="u-align-center u-text u-text-default u-text-2 center-align yahei" style='margin-top: 0px; line-height: 10px; margin-bottom: 0px'>——</p>
+    <p class="u-align-center u-text u-text-default u-text-2 center-align yahei">CHAIRMAN OF THE BOARD</p>
+  </div>
     <section class="u-clearfix u-section-2" id="sec-217c">
       <div class="u-clearfix u-sheet u-sheet-1">
         <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
@@ -22,16 +33,12 @@ include('../N1/header.php');
             <div class="u-layout-row boss-profile">
               <div class="u-container-style u-layout-cell u-left-cell u-layout-cell-1">
                 <div class="u-container-layout u-container-layout-1">
-                  <img src="images/ScreenShot2020-08-17at7.21.56AM.png" alt="" class="u-image u-image-default u-image-1" data-image-width="438" data-image-height="440">
+                  <img src="images/bosspicture.png" alt="" class="u-image u-image-default u-image-1" data-image-width="438" data-image-height="440">
                 </div>
               </div>
-              <div class="u-container-style u-layout-cell u-right-cell u-layout-cell-2 board-content">
-                <div class="u-container-layout u-container-layout-2">
-                  <h2 class="u-align-center u-text u-text-default u-text-1">
-                    <a href="http://www.gygygy.com/us.html?pageid=169611" target="http://www.gygygy.com/" class='yahei'>董事长简介</a>
-                  </h2>
-                  <p class="u-align-center u-text u-text-default u-text-2">CHAIRMAN OF THE BOARD</p>
-                  <p class="u-text u-text-default u-text-3"> 中国光影农贸市场设计研究院院长
+              <div class="u-container-style u-layout-cell u-right-cell u-layout-cell-2 board-content" style='margin-bottom: 50px'>
+                <div class="u-container-layout u-container-layout-2">                  
+                  <p class="u-text u-text-default u-text-3 yahei" style='margin-top: 0px; line-height: 45px; font-size: 16px; font-weight: 200 !Important;'> 中国光影农贸市场设计研究院院长<br>
 毕业于北京大学，后继续深造建筑与农贸设计研究，获得研究生硕士学位。
 专注研究智能农贸市场综合体20余年，研究包括农贸市场规划设计，招商策划，农贸市场管理学，农贸市场智能设备，智能农贸市场大数据，积累了丰富的农贸市场整套专业知识，先后申请六大专利，主编八册农贸市场管理体系书籍。是国内知名的农贸市场整体解决方案专家。
 杭州光影从城市商业网点整体规划布点，到政府整个创卫设计提供全方面的专业技术和商务服务。根据农贸市场地理位置和辐射人口总量设计农贸市场规模和面积。并因地制宜结合本地习惯打造一个即先进有本土化的产品。
@@ -45,13 +52,15 @@ include('../N1/header.php');
     </section>
     <section class="u-clearfix u-grey-10 u-section-3" id="sec-dca0">
       <div class="u-clearfix u-sheet u-sheet-1">
-        <h2 class="u-align-center u-text u-text-default u-text-1 yahei" style='margin-top: 50px'>杭州光影简介</h2>
-        <p class="u-align-center u-text u-text-2" style='width: 300px; margin-top: 10px; margin-bottom: 30px'>COMPANY PROFILE</p>
-        <p class="u-align-center u-text u-text-default u-text-3" style='margin-botttom: 20px'> 杭州光影建筑设计有限公司，光影农贸市场设计研究院专注农贸市场设计18年，成功案例遍及全中国32个省市县镇，是我国从事农贸市场设计行业实力雄厚的“老字号”品牌。业务矩阵包含农贸市场研究、农贸市场定位、农贸市场室内设计、建筑设计，农贸市场营运管理、农贸市场招商、
+        <h2 class="u-align-center u-text u-text-default u-text-1 yahei" style='margin-top: 50px;'>杭州光影简介</h2>
+        <p class="u-align-center u-text u-text-default u-text-2 center-align yahei" style='margin-top: 20px; line-height: 10px; margin-bottom: 0px'>——</p>
+        <p class="u-align-center u-text u-text-2 yahei" style='width: 300px; margin-top: 20px; margin-bottom: 30px'>COMPANY PROFILE</p>
+        <p class="u-align-center u-text u-text-default u-text-3 yahei" style='margin-bottom: 20px; text-align: left; line-height: 40px'> &nbsp;&nbsp;&nbsp;杭州光影建筑设计有限公司，光影农贸市场设计研究院专注农贸市场设计18年，成功案例遍及全中国32个省市县镇，是我国从事农贸市场设计行业实力雄厚的“老字号”品牌。业务矩阵包含农贸市场研究、农贸市场定位、农贸市场室内设计、建筑设计，农贸市场营运管理、农贸市场招商、
 农贸市场电商o2o，农贸市场培训指导，农贸市场智能升级、智能菜场运营管理等。专业提供农贸市场设计、菜市场设计、生鲜超市设计、农贸市场建筑设计、农贸市场效果图、农贸市场改造、菜市场效果图、菜市场改造等一整套农贸市场解决方案服务。&nbsp;&nbsp;</p>
         <img src="images/20190119173051760.jpg" alt="" class="u-expanded-width u-image u-image-default u-image-1" data-image-width="1399" data-image-height="300" style='margin-top: 30px'>
-        <p class="u-align-center u-text u-text-default u-text-4">企业顶尖团队技术成员100多人在17年时间对全国32省市的农贸市场进行了大量调研，高水准设计出符合全国本土化的星级农贸市场，科学设计摊位大小，合理分布蔬菜水果鲜肉家禽水产干货粮油业态，设计风格亲民有档次，摊位美观适用，树立多个城市五星四星三星农贸市场样板，赢得了政府，消费者，经营户一致的好评。 光影农贸市场研究院申请国家农贸市场设计专利八类，出版农贸市场管理教材八册，申请农贸市场电商系统知识产权，全国知名农贸市场尖端设计.管理.电商服务的专业服务性企业。</p>
-        <p class="u-align-left u-text u-text-default u-text-5" style='margin-top: 50px'> <span style='font-weight:bold; font-size: 110%'>发展历程</span><br style='height: 40px'>阶段一：扎根农贸行业 探索成长<br>2001：杭州光影图文设计工作室,创始人开始从事农贸行业。<br>2007：注册杭州光影投资咨询有限公司，公司业务范围扩大至：农贸市场建筑设计，农贸市场策划定位，农贸市场管理培训和顾问。 　　
+        <p class="u-align-center u-text u-text-default u-text-4 yahei" style='text-align:left; line-height: 40px'>&nbsp;&nbsp;&nbsp;企业顶尖团队技术成员100多人在17年时间对全国32省市的农贸市场进行了大量调研，高水准设计出符合全国本土化的星级农贸市场，科学设计摊位大小，合理分布蔬菜水果鲜肉家禽水产干货粮油业态，设计风格亲民有档次，摊位美观适用，树立多个城市五星四星三星农贸市场样板，赢得了政府，消费者，经营户一致的好评。 光影农贸市场研究院申请国家农贸市场设计专利八类，出版农贸市场管理教材八册，申请农贸市场电商系统知识产权，全国知名农贸市场尖端设计.管理.电商服务的专业服务性企业。</p>
+        <p class="u-align-left u-text u-text-default u-text-5 yahei" style='margin-top: 50px; line-height: 40px; margin-bottom:'> <span style='font-weight:bold; font-size: 130%;' class='yahei'>发展历程</span><br><br>
+        阶段一：扎根农贸行业 探索成长<br>2001：杭州光影图文设计工作室,创始人开始从事农贸行业。<br>2007：注册杭州光影投资咨询有限公司，公司业务范围扩大至：农贸市场建筑设计，农贸市场策划定位，农贸市场管理培训和顾问。 　　
 阶段二：专业服务，快速发展 　　&nbsp;<br>2010：杭州光影建筑设计有限公司于2010年03月29日成立，公司经过近10年的发展，1000多个市场案例字全国各地遍地开花，成绩亮眼。<br>2011：荣获浙江省公众满意质量诚信双优企业<br>2012：荣获浙江市场消费者最满意品牌单位<br>2013：公司做出重大决策，把光影打造集团目标，进而提高全国农贸市场形象，在全国注册4个子公司，杭州注册1个子公司，总部设立杭州做营运管理，子公司服务全国所有市县镇的农贸行业。荣获浙江市场金承诺示范单位。<br>2014：研发一整套生鲜超市设备体系，包括电子结算系统和先进的货架体系，成立设备研发公司。在此期间杭州光影另合股注册建筑甲级，通用全国绝大部分建筑设计和室内设计。
 　　
 阶段三：品牌升级 服务生态系统生成<br>2015：成立菜源佳佳网络科技有限公司，通过智能系统的应用打造先进智能农贸市场，引领中国农贸市场智能化大数据技应用趋势，颠覆传统农贸市场模式，同时建立并输出先进的农贸市场设计标准，高效的农贸市场管理制度，高效的招商体系等。<br>2016：光影集团中国农贸市场设计研究院在香港注册成功。<br>2017：创新发展星级农贸市场模式和农超模式设计，零售农贸市场+住宅+其他商业综合体的建筑设计，案例总数全国首位。<br>2018：全面推进智慧农贸市场的发展，智慧农贸市场服务案例全年占比超89%。<br>2019：杭州光影集团联手中国银联全城合作搭建非现金结算体系加速智能农贸市场的发展。菜源佳佳运营点十分受欢迎，规模正在极速发展壮大，在全国范围内已经成为燎原之势。
@@ -61,16 +70,18 @@ include('../N1/header.php');
     <section class="u-clearfix u-section-4" id="sec-d817">
       <div class="u-clearfix u-sheet u-sheet-1">
         <h1 class="u-align-center u-text u-text-default u-text-1 yahei" style='margin-top: 50px'> 企业文化</h1>
-        <p class="u-align-center u-text u-text-2" style='width: 300px'>ENTERPRISE CULTURE<br>
+        <p class="u-align-center u-text u-text-default u-text-2 center-align yahei" style='margin-top: 20px; line-height: 10px; margin-bottom: 0px'>——</p>
+        <p class="u-align-center u-text u-text-2 yahei" style='width: 300px'>ENTERPRISE CULTURE<br>
         </p>
-        <p class="u-align-center u-custom-font u-heading-font u-text u-text-3">创新：扎根实干，积极创新；&nbsp; &nbsp; &nbsp; &nbsp;责任：项目到人，尽职尽责；&nbsp;<br>工艺：精雕细刻，炉火纯青；&nbsp; &nbsp; &nbsp; &nbsp;情怀：积极探寻，永不放弃；&nbsp;<br>细节：认真严谨，注重细节；&nbsp; &nbsp; &nbsp; &nbsp;和谐：以人为本，和谐发展；<br>共享：价值共创，财富共享；&nbsp; &nbsp; &nbsp; &nbsp;包含：海纳百川，有容乃大；
+        <p class="u-align-center u-text u-text-3 yahei">创新：扎根实干，积极创新；&nbsp; &nbsp; &nbsp; &nbsp;责任：项目到人，尽职尽责；&nbsp;<br>工艺：精雕细刻，炉火纯青；&nbsp; &nbsp; &nbsp; &nbsp;情怀：积极探寻，永不放弃；&nbsp;<br>细节：认真严谨，注重细节；&nbsp; &nbsp; &nbsp; &nbsp;和谐：以人为本，和谐发展；<br>共享：价值共创，财富共享；&nbsp; &nbsp; &nbsp; &nbsp;包含：海纳百川，有容乃大；
         </p>
       </div>
     </section>
     <section class="u-clearfix u-section-5" id="sec-275e">
       <div class="u-clearfix u-sheet u-sheet-1">
         <h1 class="u-align-center u-text u-text-1 yahei"> 企业环境 </h1>
-        <p class="u-align-center u-text u-text-2" style='width: 300px'>ENTERPRISE ENVIRONMENT<br>
+        <p class="u-align-center u-text u-text-default u-text-2 center-align yahei" style='margin-top: 20px; line-height: 10px; margin-bottom: 0px'>——</p>
+        <p class="u-align-center u-text u-text-2 yahei" style='width: 100%'>ENTERPRISE ENVIRONMENT<br>
         </p>
         <div class="u-expanded-width u-gallery u-lightbox u-no-transition u-show-text-on-hover u-gallery-1">
           <div class="u-effect-fade u-gallery-item">
