16416e40e13d4eddc677b41cade2d3fcd7b74437
WQ <SUNREDMOBILE11@Yandex.com>
2020-09-29 11:17:10 +0800

    OK

--- api/get_applicants_number.php
+++ api/get_applicants_number.php
@@ -1,10 +1,6 @@
 <?php
 include('../N1/dbconfig.php');
 $db = getDbInstance();
-if (!isset($_REQUEST['r'])) {
-  header('location: /N1/p1.php');
-  exit;
-}
 $caseIndex = intval($_REQUEST['r']);
 $info = $db->rawQuery("select count(id) as `c` from consultation");
 
