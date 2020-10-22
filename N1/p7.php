94d1df69f57d6f0f641beb920df2dbcbfb397831
WQ <SUNREDMOBILE11@Yandex.com>
2020-10-22 21:39:12 +0800

    OK

--- N1/p7.php
+++ N1/p7.php
@@ -689,6 +689,24 @@ while (strlen($temp) > 0) {
             .replace(/"/g, "&quot;")
             .replace(/'/g, "&#039;");
       }
+      function occurrences(string, subString, allowOverlapping) {
+        string += "";
+        subString += "";
+        if (subString.length <= 0) return (string.length + 1);
+
+        var n = 0,
+            pos = 0,
+            step = allowOverlapping ? 1 : subString.length;
+
+        while (true) {
+            pos = string.indexOf(subString, pos);
+            if (pos >= 0) {
+                ++n;
+                pos += step;
+            } else break;
+        }
+        return n;
+      }
     </script>
 
     <?php include('../N1/footer.php'); ?>
