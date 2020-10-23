13f31f99319e84b7afac7adad8c0e7a551d5d65a
WQ <SUNREDMOBILE11@Yandex.com>
2020-10-23 20:32:45 +0800

    OK

--- admin/save_keyword.php
+++ admin/save_keyword.php
@@ -2,5 +2,4 @@
 if (array_key_exists('data', $_REQUEST) && sizeof(array_keys($_REQUEST['data'])) == 27) {}
 else exit;
 file_put_contents("../title_description_keywords.txt", json_encode($_REQUEST['data']));
-file_get_contents("http://localhost:8080/generatestaticpages");
-echo 'success';
+echo file_get_contents("http://localhost:8080/generatestaticpages");
