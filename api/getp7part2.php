9380314f4c27a8f36402f771bc28924412829bd0
WQ <SUNREDMOBILE11@Yandex.com>
2020-12-16 21:41:54 +0800

    OK

--- api/getp7part2.php
+++ api/getp7part2.php
@@ -1,7 +1,7 @@
 <?php
 include ("../N1/dbconfig.php");
 $db = getDbInstance();
-$data = $db->rawQuery("select * from cases where id in (select max(id) from cases where category != " . intval($_REQUEST['category']) . " group by category) order by category");
+$data = $db->rawQuery("select * from cases where hidden=false and id in (select max(id) from cases where category != " . intval($_REQUEST['category']) . " group by category) order by category");
 if (array_key_exists('increase', $_REQUEST) && $_REQUEST['increase'] == 1) {
     $db->rawQuery("update cases set browse = browse + 1 where id = " . intval($_REQUEST['caseIndex']));
 }
