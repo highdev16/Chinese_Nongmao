e0a625ecdc5ce2f2a8d48ae006e8b059c8ed3cd3
WQ <SUNREDMOBILE11@Yandex.com>
2020-12-18 20:32:41 +0800

    OK

--- admin/index1_table.php
+++ admin/index1_table.php
@@ -8,7 +8,6 @@ for ($i = 0; $i < sizeof($rows); $i++) {
     $ab = array();
 
     $ab[] =  $i + 1;
-    //<td style='width: 20%; cursor:pointer' onclick="window.location.href='index1_detail.php?id=<?php echo $rows[$i]['id']; 
     $ab[] = htmlspecialchars($rows[$i]['name']);
     $ab[] = $rows[$i]['stars'];
     $ab[] = $categoryArr[$rows[$i]['category']];
@@ -17,7 +16,7 @@ for ($i = 0; $i < sizeof($rows); $i++) {
     $ab[] = substr_count(strtolower($rows[$i]['content']), '<img ');
     $ab[] = $rows[$i]['goodone'] ? "<input type='checkbox' checked disabled onclick='setBestOne({$rows[$i]['id']})'>" : '';
     $ab[] = $rows[$i]['hidden'] ? "<input type='checkbox' checked onclick='setHidden({$rows[$i]['id']},this)'>" : "<input type='checkbox' onclick='setHidden({$rows[$i]['id']}, this)'>";
-    $ab[] = "<button type='button' onclick=\"window.location.href='index1_detail.php?id={$rows[$i]['id']}';\" class='btn btn-success' style='float:left; margin-right: 20px;'>编辑</button><button type='button' onclick='deleteThis({$rows[$i]['id']})' class='btn btn-danger' style='float:left'>删除</button>";
+    $ab[] = "<button type='button' onclick=\"window.open('index1_detail.php?id={$rows[$i]['id']}', '_blank');\" class='btn btn-success' style='float:left; margin-right: 20px;'>编辑</button><button type='button' onclick='deleteThis({$rows[$i]['id']})' class='btn btn-danger' style='float:left'>删除</button>";
     $result[] = $ab;
 } 
 
--- admin/index2_table.php
+++ admin/index2_table.php
@@ -8,12 +8,11 @@ for ($i = 0; $i < sizeof($rows); $i++) {
     $ab = array();
 
     $ab[] =  $i + 1;
-    //<td style='width: 20%; cursor:pointer' onclick="window.location.href='index1_detail.php?id=<?php echo $rows[$i]['id']; 
     $ab[] = htmlspecialchars($rows[$i]['title']);
     $ab[] = $categoryArr[$rows[$i]['category']];
     $ab[] = substr_count(strtolower($rows[$i]['content']), '<img ');
     $ab[] = $rows[$i]['goodone'] ? "<input type='checkbox' checked disabled onclick='setBestOne({$rows[$i]['id']})'>" : '';
-    $ab[] = "<button type='button' onclick=\"window.location.href='index2_detail.php?id={$rows[$i]['id']}';\" class='btn btn-success' style='float:left; margin-right: 20px;'>编辑</button><button type='button' onclick='deleteThis({$rows[$i]['id']})' class='btn btn-danger' style='float:left'>删除</button>";
+    $ab[] = "<button type='button' onclick=\"window.open('index2_detail.php?id={$rows[$i]['id']}', '_blank');\" class='btn btn-success' style='float:left; margin-right: 20px;'>编辑</button><button type='button' onclick='deleteThis({$rows[$i]['id']})' class='btn btn-danger' style='float:left'>删除</button>";
     $result[] = $ab;
 } 
 
