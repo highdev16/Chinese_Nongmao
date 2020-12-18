1fe4903236692c8bf400ce77f8ef21677e4a6267
WQ <SUNREDMOBILE11@Yandex.com>
2020-12-18 16:42:42 +0800

    OK

--- N1/footer.php
+++ N1/footer.php
@@ -165,7 +165,7 @@
       </script>
       <script type="text/javascript">document.write(unescape("%3Cspan id='cnzz_stat_icon_1279380584'%3E%3C/span%3E%3Cscript src='https://s9.cnzz.com/z_stat.php%3Fid%3D1279380584' type='text/javascript'%3E%3C/script%3E"));</script>      
   </body>
-  <script>
+<script>
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
