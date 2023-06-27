2928a96b86a02bf300595b0d3ec8d5479bc78e3e
wei <sunredmobile16@yandex.com>
2023-06-27 17:15:09 +0800

    OK

--- admin/save_keyword.php
+++ admin/save_keyword.php
@@ -1,5 +1,5 @@
 <?php
-if (array_key_exists('data', $_REQUEST) && sizeof(array_keys($_REQUEST['data'])) == 27) {}
+if (array_key_exists('data', $_REQUEST) && sizeof(array_keys($_REQUEST['data'])) == 28) {}
 else exit;
 file_put_contents("../title_description_keywords.txt", json_encode($_REQUEST['data']));
 echo file_get_contents("http://localhost:8080/generatestaticpages");
